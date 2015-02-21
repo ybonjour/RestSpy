5d8d6cd9edcd712db509ce3defcf05523048437d
Yves Bonjour <yves.bonjour@soundcloud.com>
2015-02-21 19:30:00 +0100

    Added missing file

---
+++ lib/rest-spy/model/Double.rb
@@ -0,0 +1,20 @@
+module RestSpy
+  module Model
+    class Double
+      def initialize(pattern, body, status_code, headers)
+        raise ArgumentError unless pattern && body
+
+        @pattern = pattern
+        @status_code = status_code || 200
+        @headers = headers  || {}
+        @body = body
+      end
+
+      attr_reader :status_code, :headers, :body
+
+      def matches(s)
+        (@pattern =~ s) != nil
+      end
+    end
+  end
+end
