c4931bd7fe0e4bc9077cdc72388336e26f250319
Yves Bonjour <yves.bonjour@soundcloud.com>
2015-02-21 21:32:42 +0100

    Replaced double quotes with single quotes

--- spec/model/double_registry_spec.rb
+++ spec/model/double_registry_spec.rb
@@ -3,7 +3,7 @@ require 'rest-spy/model'
 module RestSpy
   module Model
     describe DoubleRegistry do
-      let(:double) { Double.new(/\/test/, "body", nil, nil)}
+      let(:double) { Double.new(/\/test/, 'body', nil, nil)}
       let(:registry) { DoubleRegistry.new }
 
       it "should find a registered Double" do
@@ -30,7 +30,7 @@ module RestSpy
 
       it "should return first Double if two Doubles are matching" do
         registry.register(double)
-        double2 = Double.new(/\/test/, "body", nil, nil)
+        double2 = Double.new(/\/test/, 'body2', nil, nil)
         registry.register(double2)
 
         result = registry.find_double_for_endpoint('/test')
