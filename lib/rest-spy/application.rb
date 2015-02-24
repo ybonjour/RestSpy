901480be6113a0dae9f999a218576425b8dacfe2
Yves Bonjour <yves.bonjour@soundcloud.com>
2015-02-24 09:58:39 +0100

    Added log output

--- lib/rest-spy/application.rb
+++ lib/rest-spy/application.rb
@@ -13,6 +13,8 @@ module RestSpy
     post '/doubles' do
       return 400 unless params[:pattern] && params[:body]
 
+      $stderr.puts "[Double: #{params[:pattern]}]"
+
       d = Model::Double.new(params[:pattern], params[:body], params[:status_code], params[:headers])
       @@DOUBLES.register(d, request.port)
 
@@ -20,11 +22,14 @@ module RestSpy
     end
 
     delete '/doubles/all' do
+      $stderr.puts "[Clear all doubles]"
       @@DOUBLES.reset(request.port)
     end
 
     post '/proxies' do
       return 400 unless params[:pattern] && params[:redirect_url]
+
+      $stderr.puts "[Proxy: #{params[:pattern]} -> #{params[:redirect_url]}]"
 
       p = Model::Proxy.new(params[:pattern], params[:redirect_url])
       @@PROXIES.register(p, request.port)
@@ -38,10 +43,13 @@ module RestSpy
       proxy = @@PROXIES.find_for_endpoint(capture, request.port)
 
       if double
+        $stderr.puts "[Request #{capture} -> Double: #{double.status_code}]"
         respond(double.status_code, double.headers, double.body)
       elsif proxy
         remote_response = ProxyServer.execute_remote_request(request, proxy.redirect_url, env)
-        respond(remote_response.status, remote_response.headers, remote_response.body)
+        $stderr.puts "[Request #{capture} -> Proxy #{remote_response.status}]"
+        body = rewrite(remote_response.body)
+        respond(remote_response.status, remote_response.headers, body)
       else
         404
       end
@@ -52,6 +60,12 @@ module RestSpy
       headers(headers)
       body(body)
       status(status_code)
+    end
+
+    def rewrite(input)
+      from = 'http://api.soundcloud.dev:8989'
+      to = 'http://localhost:5678'
+      input.gsub(/#{from}/, "#{to}")
     end
   end
 end
