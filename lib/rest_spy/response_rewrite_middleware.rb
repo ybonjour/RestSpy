9640f35a4fbd8285d479bbc46bd42d433bc37429
Yves Bonjour <yves.bonjour@soundcloud.com>
2015-03-12 12:25:36 +0100

    Fixed problems with content-length header

--- lib/rest_spy/response_rewrite_middleware.rb
+++ lib/rest_spy/response_rewrite_middleware.rb
@@ -4,6 +4,9 @@ require_relative 'response_rewriter'
 module RestSpy
   class ResponseRewriteMiddleware < Faraday::Middleware
 
+    CONTENT_LENGTH = 'Content-Length'.freeze
+    CONTENT_ENCODING = 'Content-Encoding'.freeze
+
     def initialize(app, options=[])
       super(app)
       @rewriter = ResponseRewriter.new(options[:rewrites])
@@ -12,8 +15,10 @@ module RestSpy
     def call(request_env)
       @app.call(request_env).on_complete do |response_env|
         if response_env[:body]
-          response_env[:body] = rewriter.rewrite(response_env[:body], response_env[:response_headers]['Content-Encoding'])
-          response_env[:response_headers]['Content-Length'] = "#{response_env[:body].length}"
+          response_env[:body] = rewriter.rewrite(response_env[:body], response_env[:response_headers][CONTENT_ENCODING])
+          if response_env[:response_headers][CONTENT_LENGTH]
+            response_env[:response_headers][CONTENT_LENGTH] = "#{response_env[:body].length}"
+          end
         end
       end
     end
